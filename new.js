fbc109a5ae0efd0fe52c6f18f72c01bba23cd649
Mia Desiree Aburto <149914416+maburto7@users.noreply.github.com>
2025-01-08 11:45:11 -0800

    Create new.js

---
+++ new.js
@@ -0,0 +1,3 @@
+window.addEventListener('resize', function() {
+  document.querySelector('.slide').style.height = window.innerHeight + 'px'; // dynamically resize the sidebar to take up entire screen
+});
